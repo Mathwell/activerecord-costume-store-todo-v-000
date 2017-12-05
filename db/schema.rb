e31a86893215bfb2254dd6f0a157a65dca3caf0a
Olena Ageyeva <eageeva@hotmail.com>
2017-12-05 01:43:37 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.float "price"
     t.integer "size"
     t.string "imageurl"
-  end
-
-  create_table "hounted_houses", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "theme"
-    t.float "price"
-    t.boolean "family_friendly?"
-    t.datetime "opening"
-    t.datetime "closing"
-    t.text "description"
   end
 
   create_table "stores", force: :cascade do |t|
