f1fbfacc69dcbc4c86ca0fa30cfcd040dbada033
Olena Ageyeva <eageeva@hotmail.com>
2017-12-05 01:34:16 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,21 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
+  create_table "costume_stores", force: :cascade do |t|
+    t.string "name"
+    t.text "location"
+    t.integer "inventory"
+    t.integer "employees"
+    t.boolean "in_business"
+    t.datetime "opening"
+    t.datetime "closing"
+  end
+
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.float "price"
     t.integer "size"
-    t.string "imageurl"
+    t.string "image_url"
   end
 
   create_table "hounted_houses", force: :cascade do |t|
@@ -28,16 +38,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.datetime "opening"
     t.datetime "closing"
     t.text "description"
-  end
-
-  create_table "stores", force: :cascade do |t|
-    t.string "name"
-    t.text "location"
-    t.integer "inventory"
-    t.integer "employees"
-    t.boolean "in_business"
-    t.datetime "opening"
-    t.datetime "closing"
   end
 
 end
