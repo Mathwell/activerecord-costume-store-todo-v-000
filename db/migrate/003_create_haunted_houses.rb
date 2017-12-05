1531d14af187adfa1eab12c4ef9129cc32eed004
Olena Ageyeva <eageeva@hotmail.com>
2017-12-05 01:43:32 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,9 +6,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.string    :location
       t.string :theme
       t.float  :price
-      t.boolean  :family_friendly
-      t.datetime :opening_date
-      t.datetime :closing_date
+      t.boolean  :family_friendly?
+      t.datetime :opening
+      t.datetime :closing
       t.text  :description
     end
   end
