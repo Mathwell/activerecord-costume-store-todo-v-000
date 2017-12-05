040c7396138e2cbd3c4a61709d2c00b79cab02ed
Olena Ageyeva <eageeva@hotmail.com>
2017-12-05 01:28:15 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.boolean  :family_friendly?
       t.datetime :opening
       t.datetime :closing
-      t.string  :description
+      t.text  :description
     end
   end
 end
